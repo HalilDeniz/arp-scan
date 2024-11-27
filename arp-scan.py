0694f8a0e30693254475166680d6b0e00e104d12
halil ibrahim deniz <62566319+HalilDeniz@users.noreply.github.com>
2024-11-27 17:16:30 +0300

    Update arp-scan.py

--- arp-scan.py
+++ arp-scan.py
@@ -7,28 +7,43 @@ Created March 2019
 @author: othornew
 """
 
-import sys
+import argparse
 from datetime import datetime
-from scapy.all import srp,Ether,ARP,conf 
+from scapy.all import srp, Ether, ARP, conf
+from colorama import Fore, Style
 
+class ARPScanner:
+    def __init__(self, interface, ips):
+        self.interface = interface
+        self.ips = ips
 
-def arp_scan(interface, ips):
+    def scan(self):
+        print(Fore.YELLOW + "[*] Scanning..." + Style.RESET_ALL) 
+        start_time = datetime.now()
 
-	print("[*] Scanning...") 
-	start_time = datetime.now()
+        conf.verb = 0 
+        ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=self.ips), 
+                         timeout=2, 
+                         iface=self.interface,
+                         inter=0.1)
 
-	conf.verb = 0 
-	ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), 
-		     timeout = 2, 
-		     iface = interface,
-		     inter = 0.1)
+        print(Fore.GREEN + "\n[*] IP - MAC" + Style.RESET_ALL) 
+        for snd, rcv in ans: 
+            print(Fore.CYAN + rcv.sprintf(r"%ARP.psrc% - %Ether.src%") + Style.RESET_ALL)
+        
+        stop_time = datetime.now()
+        total_time = stop_time - start_time 
+        print(Fore.YELLOW + "\n[*] Scan Complete. Duration: " + str(total_time) + Style.RESET_ALL)
 
-	print ("\n[*] IP - MAC") 
-	for snd,rcv in ans: 
-		print(rcv.sprintf(r"%ARP.psrc% - %Ether.src%"))
-	stop_time = datetime.now()
-	total_time = stop_time - start_time 
-	print("\n[*] Scan Complete. Duration:", total_time)
+def main():
+    parser = argparse.ArgumentParser(description="ARP Scanner")
+    parser.add_argument("interface", help="Network interface to use")
+    parser.add_argument("ips", help="IP range to scan (e.g., 192.168.1.0/24)")
+    
+    args = parser.parse_args()
+    
+    scanner = ARPScanner(args.interface, args.ips)
+    scanner.scan()
 
 if __name__ == "__main__":
-    arp_scan(sys.argv[1], sys.argv[2])
+    main()
